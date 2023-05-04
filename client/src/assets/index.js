3360d085b0d202a87a66f5c8552fb5aea7e94bb8
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-05 03:52:24 +0800

    update navbar

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,8 +2,18 @@ import trash from './trash-can.png';
 import save from './floppy-disk.png';
 import edit from './write.png';
 
+import bookkeep from './navbar/bookkeep.svg'
+import analyze from './navbar/analyze.svg'
+import analyze_dark from './navbar/analyze_dark.png'
+import home from './navbar/home.svg'
+
+
 export {
     trash,
     save,
     edit,
+    bookkeep,
+    analyze,
+    analyze_dark,
+    home,
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  bookkeep,
+  analyze,
+  analyze_dark,
+  home,
+} from "../assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between px-10 gap-3 h-12 bg-primary-200">
+      <div className="flex items-center text-xl font-mono font-bold">
+        Olulu Stock
+      </div>
+      <div className="flex">
+        <Link to={"/"} className="flex gap-1 items-center px-2 hover:bg-primary-300">
+          <img className="w-4 h-4" src={home} alt="home" />
+          <p>Home</p>
+        </Link>
+        <Link to={"/symbol"} className="flex gap-1 items-center px-2 hover:bg-primary-300">
+          <img className="w-4 h-4" src={analyze} alt="analyze" />
+          <p>Analyze</p>
+        </Link>
+        <Link to={"/add"} className="flex gap-1 items-center px-2 hover:bg-primary-300">
+          <img className="w-4 h-4" src={bookkeep} alt="bookkeep" />
+          <p>Bookkeep</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;

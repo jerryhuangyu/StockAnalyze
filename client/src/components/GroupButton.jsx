7a9056c966a19c2ea45bff149aa7c13893f55317
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-06 00:10:56 +0800

    udpate group button

--- client/src/components/GroupButton.jsx
+++ client/src/components/GroupButton.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const GroupButton = ({ names }) => {
   return (
-    <div className="inline-flex rounded-md shadow-sm">
+    <div className="inline-flex rounded-md gap-[2px] shadow-sm">
       {names.map((name, index) => {
         return (
           <Link
             key={index + "btn"}
             to={`/symbol/${name.symbol}`}
-            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 border-y
+            className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-gray-200 drop-shadow
             hover:bg-primary-50 hover:text-primary-600 focus:z-10 focus:ring-2 focus:ring-primary-500 focus:text-primary-600
-            first:rounded-l-md last:rounded-r-md first:border last:border"
+            first:rounded-l-lg first:rounded-r-sm rounded-sm last:rounded-r-lg last:rounded-l-sm"
             type="button"
           >
             {name.symbol}

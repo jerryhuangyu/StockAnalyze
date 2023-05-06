543dc660b5e26834b622427aa889854475947dfa
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-06 12:01:19 +0800

    update navbar gap

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 import { bookkeep, analyze, home, menu } from "../assets";
 
 const Navbar = () => {
   const [menuToggle, setMenuToggle] = useState(false);
 
   return (
-    <div className="flex justify-between px-10 gap-3 h-12 bg-primary-50 ">
-      <div className="w-[100%] h-10 z-[-1] absolute bg-primary-200 top-[10px] left-0"></div>
+    <div className="flex justify-between px-10 h-[60px] bg-primary-50 ">
       <div className="flex items-center text-xl font-mono font-bold cursor-pointer">
         <Link to={"/"}>S-Insight</Link>
       </div>
@@ -16,21 +16,21 @@ const Navbar = () => {
       <div className="sm:flex hidden">
         <Link
           to={"/"}
-          className="flex gap-1 items-center px-2 hover:bg-primary-300"
+          className="flex gap-2 items-center px-2 hover:bg-primary-300"
         >
           <img className="w-4 h-4" src={home} alt="home" />
           <p>Home</p>
         </Link>
         <Link
           to={"/symbol"}
-          className="flex gap-1 items-center px-2 hover:bg-primary-300"
+          className="flex gap-2 items-center px-2 hover:bg-primary-300"
         >
           <img className="w-4 h-4" src={analyze} alt="analyze" />
           <p>Analyze</p>
         </Link>
         <Link
           to={"/add"}
-          className="flex gap-1 items-center px-2 hover:bg-primary-300"
+          className="flex gap-2 items-center px-2 hover:bg-primary-300"
         >
           <img className="w-4 h-4" src={bookkeep} alt="bookkeep" />
           <p>Bookkeep</p>

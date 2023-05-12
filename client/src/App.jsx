bae6c3b1524ed4fa84ebb048600fedde01c190a5
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-12 18:58:48 +0800

    add login function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,28 +8,13 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 function App() {
   const [user, setUser] = useState({});
-  const handleCallbackResponse = (res) => {
-    console.log("Encoded JWT ID token: " + res.credential);
-    var userObject = jwt_decode(res.credential);
-    console.log(userObject);
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
-  };
+
   const handleSignOut = (e) => {
     setUser({});
     document.getElementById("signInDiv").hidden = false;
   };
   useEffect(() => {
     // global google
-    google.accounts.id.initialize({
-      client_id:
-        "759855032131-pn9b9ko6glu6925r5nna6dp9salgdneb.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
   }, []);
 
   return (
@@ -43,23 +28,13 @@ function App() {
           <Route path="/symbol" element={<Symbol />}>
             <Route path=":symbol" element={<Chart />} />
           </Route>
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={<Login user={user} setUser={setUser} />}
+          />
         </Routes>
       </div>
       <Footer />
-      {Object.keys(user).length !== 0 && (
-        <div className="w-full flex gap-3 items-center">
-          <img src={user.picture} alt="user" className="w-8 aspect-square" />
-          <h3>{user.name}</h3>
-          <button
-            className="h-8 text-white px-3 bg-black"
-            onClick={(e) => handleSignOut(e)}
-          >
-            Sign out
-          </button>
-        </div>
-      )}
-      <div id="signInDiv"></div>
     </BrowserRouter>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import { render } from "react-dom";
 
-const Login = () => {
+const LoginOptions = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center gap-6 border w-[280px] py-6">
+      <h3>Login Options</h3>
+      <div id="signInDiv"></div>
+    </div>
+  );
+};
 
-export default Login
+const LoginWellcome = ({ user, handleSignOut }) => {
+  // console.log(user);
+  return (
+    <div className="w-full flex flex-col gap-3 items-center">
+      <img src={user.picture} alt="user" className="w-20 aspect-square" />
+      <h3>{user.name}</h3>
+      <button
+        className="cursor-pointer h-8 w-[100px] px-3 bg-secondary-300 rounded-md"
+        onClick={() => handleSignOut()}
+      >
+        Sign out
+      </button>
+      <button className="cursor-pointer bg-primary-100 h-8 w-[100px] rounded-md">
+        Home
+      </button>
+    </div>
+  );
+};
+
+const Login = ({ user, setUser }) => {
+  const [isLogin, setIsLogin] = useState(Object.keys(user).length);
+  const initGoogleLogin = () => {
+    google.accounts.id.initialize({
+      client_id:
+        "759855032131-pn9b9ko6glu6925r5nna6dp9salgdneb.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+  };
+  const renderGoogleLoginButton = () => {
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  };
+  const handleCallbackResponse = (res) => {
+    var userObject = jwt_decode(res.credential);
+    setUser(userObject);
+    setIsLogin(true);
+  };
+  const handleSignOut = () => {
+    setUser({});
+    setIsLogin(false);
+  };
+
+  useEffect(() => {
+    initGoogleLogin();
+    renderGoogleLoginButton();
+  }, []);
+  useEffect(() => {
+    renderGoogleLoginButton();
+  }, [isLogin]);
+
+  return (
+    <div className="h-screen mt-16">
+      {!isLogin && <LoginOptions />}
+      {Object.keys(user).length !== 0 && (
+        <LoginWellcome user={user} handleSignOut={handleSignOut} />
+      )}
+    </div>
+  );
+};
+
+export default Login;

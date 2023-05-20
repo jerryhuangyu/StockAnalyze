7d233fb6d0d277e4e0e6d65427b6ebfe6239d3ae
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-20 17:56:22 +0800

    fix footer color

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div
-      className="text-center p-3 mt-6"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-    >
-      &copy; {new Date().getFullYear()} Copyright:{" "}
-      <a className="text-dark" href="https://portfolio-peach-one-10.vercel.app/">
-        Jerry Huang
-      </a>
-    </div>
+    <>
+      <div
+        className="text-center p-3 mt-6 bg-zinc-300"
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a
+          className="text-dark"
+          href="https://portfolio-peach-one-10.vercel.app/"
+        >
+          Jerry Huang
+        </a>
+      </div>
+    </>
   );
 };
 

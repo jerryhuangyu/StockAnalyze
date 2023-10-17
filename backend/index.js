406e65890f9b6cc81decc4e7c9a25a7e9108923f
jerryhuangyu <jerry0932619628@gmail.com>
2023-10-17 20:30:57 +0800

    update: Setup vercel serverless function

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 
-import { appTestRouter, stockRouter, stocksRouter, symbolRouter } from './routes/index.js';
+import {
+  appTestRouter,
+  stockRouter,
+  stocksRouter,
+  symbolRouter,
+} from "./routes/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/stocks', stocksRouter);
-app.use('/stock', stockRouter);
-app.use('/symbol', symbolRouter);
-app.use('/test', appTestRouter);
+app.use("/stocks", stocksRouter);
+app.use("/stock", stockRouter);
+app.use("/symbol", symbolRouter);
+app.use("/test", appTestRouter);
 
-app.listen(8080, () => console.log("backend is running"))
+app.listen(8080, () => console.log("backend is running"));
+
+export default app;

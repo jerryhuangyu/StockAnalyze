6a9a054a11d9e832dc9503a39fef3de55d6fa2de
jerryhuangyu <jerry0932619628@gmail.com>
2023-10-18 09:49:07 +0800

    update: Setup token in header

--- client/src/services/finnhubStock.js
+++ client/src/services/finnhubStock.js
@@ -1,16 +1,20 @@
 import { baseApi } from "./baseApi";
 
 const endpointUrl = import.meta.env.VITE_FINNHUB_SERVER_URL;
+const token = import.meta.env.VITE_FINNHUB_APIKEY;
 
 const finnhubStockApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAapl: builder.query({
-            query: () => `${endpointUrl}quote?symbol=AAPL&token=${import.meta.env.VITE_FINNHUB_APIKEY}`
-        }),
-        getSymbol: builder.query({
-            query: (symbol) => `${endpointUrl}quote?symbol=${symbol}&token=${import.meta.env.VITE_FINNHUB_APIKEY}`
-        }),
-    })
-})
+  endpoints: (builder) => ({
+    getAapl: builder.query({
+      query: () => `${endpointUrl}quote?symbol=AAPL&token=${token}`,
+    }),
+    getSymbol: builder.query({
+      query: (symbol) => ({
+        url: `${endpointUrl}quote?symbol=${symbol}`,
+        headers: { "X-Finnhub-Token": `${token}` },
+      }),
+    }),
+  }),
+});
 
 export const { useGetAaplQuery, useGetSymbolQuery } = finnhubStockApi;

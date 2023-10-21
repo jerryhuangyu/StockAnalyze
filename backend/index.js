bfb30ce87705b7cbf6a5cd65899e52ab228feae6
jerryhuangyu <jerry0932619628@gmail.com>
2023-10-21 17:20:58 +0800

    add: Setup auth0 middleware for Express

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import { auth } from "express-oauth2-jwt-bearer";
+import * as dotenv from "dotenv";
+import {
+  appTestRouter,
+  stockRouter,
+  stocksRouter,
+  symbolRouter,
+} from "./routes/index.js";
 
-import { appTestRouter, stockRouter, stocksRouter, symbolRouter } from './routes/index.js';
+dotenv.config();
+
+const jwtCheck = auth({
+  audience: process.env.AUTH_AUDIENCE,
+  issuerBaseURL: process.env.AUTH_ISSUER_BASE_URL,
+  tokenSigningAlg: process.env.AUTH_ALG,
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(jwtCheck);
 
-app.use('/stocks', stocksRouter);
-app.use('/stock', stockRouter);
-app.use('/symbol', symbolRouter);
-app.use('/test', appTestRouter);
+app.use("/stocks", stocksRouter);
+app.use("/stock", stockRouter);
+app.use("/symbol", symbolRouter);
+app.use("/test", appTestRouter);
 
-app.listen(8080, () => console.log("backend is running"))
+app.listen(8080, () => console.log("backend is running"));

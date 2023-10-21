709833690181e8d1d2dd794609bc8d65f63159ff
jerryhuangyu <jerry0932619628@gmail.com>
2023-10-21 21:38:47 +0800

    add: Create line wave loader for card

--- client/src/components/HomeCard.jsx
+++ client/src/components/HomeCard.jsx
@@ -1,13 +1,23 @@
+import { LineWave } from "react-loader-spinner";
+
+const LineWaveLoader = () => (
+  <div className="relative h-12 overflow-hidden">
+    <div className="absolute bgout -top-10 -left-4">
+      <LineWave color="#8CD4E5" />
+    </div>
+  </div>
+);
+
 const HomeCard = ({ header, description, icon }) => {
   return (
     <div className="bg-primary-50 shadow rounded-lg flex justify-between items-center px-9">
       <div className="flex flex-col">
         <h2 className="text-primary-out font-bold drop-shadow-sm text-5xl">
-          {header !== undefined ? String(header) : "..."}
+          {header !== undefined ? String(header) : <LineWaveLoader />}
         </h2>
         <p className="text-primary-200">{description}</p>
       </div>
-      <img className="w-10 mr-2" src={icon} alt="" />
+      <img className="w-10 mr-2" src={icon} alt={header} />
     </div>
   );
 };

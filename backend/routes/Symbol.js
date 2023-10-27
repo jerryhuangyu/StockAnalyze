4d8db2b8bf66c55c6e9d0fcc5910a43dae485cc5
jerryhuangyu <jerry0932619628@gmail.com>
2023-10-28 07:05:58 +0800

    update: Setup table name for multi tenant

--- backend/routes/Symbol.js
+++ backend/routes/Symbol.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import db from '../databases/mysql.js';
+import express from "express";
+import db from "../databases/mysql.js";
+import { tableName } from "../const.js";
 
 const symbolRouter = express.Router();
 
-symbolRouter.get('/:symbol', (req, res) => {
-    const symbol = req.params.symbol;
-    const query = 'SELECT * FROM olulu WHERE symbol = ?';
+symbolRouter.get("/:symbol", (req, res) => {
+  const symbol = req.params.symbol;
+  const query = `SELECT * FROM ${tableName} WHERE symbol = ?`;
 
-    db.query(query, [symbol], (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
+  db.query(query, [symbol], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 export default symbolRouter;

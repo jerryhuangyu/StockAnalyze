8333cf46da4481deef5dac41975e93ef0033509b
jerryhuangyu <jerry0932619628@gmail.com>
2024-03-05 14:50:28 +0800

    refactor: Create navbar component for mobile and desktop

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,46 +6,40 @@ import { LoginButton, NavbarLink } from "./";
 import { bookkeep, analyze, profileIcon, home, menu, guide } from "../assets";
 import { showGuide } from "../utils/guide";
 
-const Navbar = () => {
+const DesktopNavbar = () => (
+  <div className="sm:flex hidden">
+    {/* TODO: naming conflict profile and login */}
+    <NavbarLink icon={home} linkName={"Home"} linkTo={"/"} />
+    <NavbarLink icon={profileIcon} linkName={"Profile"} linkTo={"/login"} />
+    <NavbarLink
+      icon={analyze}
+      linkName={"Analyze"}
+      linkTo={"/symbol"}
+      id="step-three"
+    />
+    <NavbarLink
+      icon={bookkeep}
+      linkName={"Bookkeep"}
+      linkTo={"/add"}
+      id="step-four"
+    />
+    <NavbarLink
+      icon={guide}
+      linkName={"Guide Me"}
+      linkTo={"/"}
+      onClick={(e) => {
+        showGuide(false);
+        e.stopPropagation();
+      }}
+    />
+    <LoginButton />
+  </div>
+);
+
+const MobileNavbar = () => {
   const [menuToggle, setMenuToggle] = useState(false);
-  const isSmall = useMediaQuery("(max-width: 640px)");
-
   return (
-    <div className="flex justify-between px-[5%] lg:px-10 h-[60px] bg-primary-50 ">
-      <div className="flex items-center text-xl font-mono font-bold cursor-pointer">
-        <Link to={"/"}>S-Insight</Link>
-      </div>
-
-      {/* menu for beyond sm */}
-      <div className="sm:flex hidden">
-        {/* TODO: naming conflict profile and login */}
-        <NavbarLink icon={home} linkName={"Home"} linkTo={"/"} />
-        <NavbarLink icon={profileIcon} linkName={"Profile"} linkTo={"/login"} />
-        <NavbarLink
-          icon={analyze}
-          linkName={"Analyze"}
-          linkTo={"/symbol"}
-          id="step-three"
-        />
-        <NavbarLink
-          icon={bookkeep}
-          linkName={"Bookkeep"}
-          linkTo={"/add"}
-          id="step-four"
-        />
-        <NavbarLink
-          icon={guide}
-          linkName={"Guide Me"}
-          linkTo={"/"}
-          onClick={(e) => {
-            showGuide(isSmall);
-            e.stopPropagation();
-          }}
-        />
-        <LoginButton />
-      </div>
-
-      {/* menu for sm */}
+    <>
       <div className="flex items-center sm:hidden">
         <img
           className="w-5 cursor-pointer"
@@ -54,7 +48,6 @@ const Navbar = () => {
           onClick={() => setMenuToggle(!menuToggle)}
         />
       </div>
-
       <div
         className={`${
           menuToggle ? "left-0" : "left-[-100%]"
@@ -94,7 +87,7 @@ const Navbar = () => {
           linkName={"Guide Me"}
           onClick={(e) => {
             setMenuToggle(!menuToggle);
-            showGuide(isSmall);
+            showGuide(true);
             e.stopPropagation();
           }}
           className="gap-3 text-lg"
@@ -104,6 +97,18 @@ const Navbar = () => {
           onClick={() => setMenuToggle(!menuToggle)}
         />
       </div>
+    </>
+  );
+};
+
+const Navbar = () => {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+  return (
+    <div className="flex justify-between px-[5%] lg:px-10 h-[60px] bg-primary-50 ">
+      <div className="flex items-center text-xl font-mono font-bold cursor-pointer">
+        <Link to={"/"}>S-Insight</Link>
+      </div>
+      {isMobile ? <MobileNavbar /> : <DesktopNavbar />}
     </div>
   );
 };

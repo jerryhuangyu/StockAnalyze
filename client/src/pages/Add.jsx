fca021609a3f9432a88122457a79f0f82735b730
jerryhuangyu <jerry0932619628@gmail.com>
2024-03-06 01:04:28 +0800

    style: Center input form with footer

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -3,7 +3,11 @@ import { StockForm } from "../components";
 const Add = () => {
   const title = "Add New Order History";
 
-  return <StockForm title={title} />;
+  return (
+    <div className="w-full h-[calc(100vh-132px)] flex items-center justify-center">
+      <StockForm title={title} />
+    </div>
+  );
 };
 
 export default Add;
--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -3,7 +3,11 @@ import { StockForm } from "../components";
 const Update = () => {
   const title = "Update Order Info";
 
-  return <StockForm title={title} isUpdate />;
+  return (
+    <div className="w-full h-[calc(100vh-132px)] flex items-center justify-center">
+      <StockForm title={title} isUpdate />
+    </div>
+  );
 };
 
 export default Update;
